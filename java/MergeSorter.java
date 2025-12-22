c50f305a21f9cc672376c4bf76fe981e0ee15f59
Carl Lundholm <carl_0221@hotmail.se>
2025-12-22 10:12:42 +0100

    Add MergeSorter implementation using IntSort interface with hybrid merge-insertion logic

---
+++ java/MergeSorter.java
@@ -0,0 +1,42 @@
+public class MergeSorter implements IntSort {
+
+    private int M = 15;
+    private IntSort insertionSorter = new InsertionSort();
+
+    public void sort(int[] a, int[] temp, int lo, int hi) {
+        if (hi <= lo) return;
+
+        if (hi - lo <= M) {
+            insertionSorter.sort(a);
+            return;
+        }
+
+        int mid = lo + (hi - lo) / 2;
+
+        sort(a, temp, lo, mid);
+        sort(a, temp, mid+1, hi);
+
+        if (a[mid] <= a[mid+1]) return;
+
+        merge(a, temp, lo, mid, hi);
+    }
+
+    @Override
+    public void sort(int[] a) {
+        int[] temp = new int[a.length];
+        sort(a, temp, 0, a.length - 1);
+    }
+
+    private void merge(int[] a, int[] temp, int lo, int mid, int hi) {
+        for (int k = lo; k <= hi; k++) temp[k] = a[k];
+
+        int i = lo, j = mid+1;
+
+        for (int k = lo; k <= hi; k++) {
+            if      (i > mid)                   a[k] = temp[j++];
+            else if (j > hi)                    a[k] = temp[i++];
+            else if (temp[j] < temp[i])         a[k] = temp[j++];
+            else                                a[k] = temp[i++];
+        }
+    }
+}
